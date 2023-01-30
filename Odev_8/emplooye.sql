75b07553ea8fd5bca9701f23aed2b024161dca6a
= <=>
2023-01-30 14:21:22 +0300

    Ödev 8 yükleme

---
+++ Odev_8/emplooye.sql
@@ -0,0 +1,57 @@
+create table emplooye (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	gender VARCHAR(50)
+);
+insert into emplooye (id, first_name, last_name, email, gender) values (1, 'Nathan', 'Bohills', 'nbohills0@digg.com', 'Bigender');
+insert into emplooye (id, first_name, last_name, email, gender) values (2, 'Rusty', 'Deighan', 'rdeighan1@elegantthemes.com', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (3, 'Paola', 'Roskeilly', 'proskeilly2@artisteer.com', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (4, 'Donelle', 'Hitscher', 'dhitscher3@g.co', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (5, 'Laurene', 'Caroline', 'lcaroline4@uiuc.edu', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (6, 'Sophey', 'Arden', 'sarden5@topsy.com', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (7, 'Moritz', 'Waite', 'mwaite6@taobao.com', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (8, 'Pall', 'Ebunoluwa', 'pebunoluwa7@adobe.com', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (9, 'Kali', 'Ollerhad', 'kollerhad8@slate.com', 'Agender');
+insert into emplooye (id, first_name, last_name, email, gender) values (10, 'Sheridan', 'Duxbury', 'sduxbury9@wordpress.org', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (11, 'Alasteir', 'Lacheze', 'alachezea@utexas.edu', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (12, 'Ferdinande', 'Owens', 'fowensb@mysql.com', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (13, 'Francene', 'Howison', 'fhowisonc@whitehouse.gov', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (14, 'Jamie', 'Espadas', 'jespadasd@ebay.com', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (15, 'Tammi', 'Mion', 'tmione@list-manage.com', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (16, 'Erwin', 'Stepto', 'esteptof@tiny.cc', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (17, 'Buddy', 'Reen', 'breeng@biblegateway.com', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (18, 'Alfredo', 'Vreede', 'avreedeh@myspace.com', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (19, 'Rachele', 'Simonutti', 'rsimonuttii@independent.co.uk', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (20, 'Tomkin', 'Habbal', 'thabbalj@army.mil', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (21, 'Reggie', 'Georg', 'rgeorgk@netvibes.com', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (22, 'Jyoti', 'Davison', 'jdavisonl@cafepress.com', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (23, 'Minta', 'Mathieu', 'mmathieum@lycos.com', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (24, 'Pepito', 'Lacroix', 'placroixn@google.fr', 'Non-binary');
+insert into emplooye (id, first_name, last_name, email, gender) values (25, 'Odelia', 'Pestell', 'opestello@over-blog.com', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (26, 'Cal', 'Anyon', 'canyonp@pbs.org', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (27, 'Javier', 'Carvil', 'jcarvilq@webs.com', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (28, 'Min', 'Bollis', 'mbollisr@ftc.gov', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (29, 'Peter', 'Pettko', 'ppettkos@opera.com', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (30, 'Brinna', 'Ugo', 'bugot@ifeng.com', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (31, 'Kareem', 'Jahnel', 'kjahnelu@digg.com', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (32, 'Cooper', 'Francillo', 'cfrancillov@dmoz.org', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (33, 'Rodie', 'Fenton', 'rfentonw@booking.com', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (34, 'Keary', 'Braben', 'kbrabenx@parallels.com', 'Bigender');
+insert into emplooye (id, first_name, last_name, email, gender) values (35, 'Minette', 'Pallis', 'mpallisy@dmoz.org', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (36, 'Jolee', 'Di Angelo', 'jdiangeloz@github.com', 'Genderfluid');
+insert into emplooye (id, first_name, last_name, email, gender) values (37, 'Bank', 'Olivia', 'bolivia10@ibm.com', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (38, 'Charil', 'Knowlden', 'cknowlden11@theglobeandmail.com', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (39, 'Humphrey', 'Scott', 'hscott12@forbes.com', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (40, 'Bobbette', 'Spillman', 'bspillman13@spotify.com', 'Bigender');
+insert into emplooye (id, first_name, last_name, email, gender) values (41, 'Milli', 'Deek', 'mdeek14@zimbio.com', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (42, 'Millard', 'Bims', 'mbims15@techcrunch.com', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (43, 'Melosa', 'Hellwing', 'mhellwing16@ox.ac.uk', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (44, 'Orton', 'Sattin', 'osattin17@ask.com', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (45, 'Conney', 'Bilovsky', 'cbilovsky18@rediff.com', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (46, 'Melany', 'Fosh', 'mfosh19@jiathis.com', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (47, 'Herby', 'Allchin', 'hallchin1a@mlb.com', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (48, 'Alexandro', 'Derks', 'aderks1b@rambler.ru', 'Male');
+insert into emplooye (id, first_name, last_name, email, gender) values (49, 'Florella', 'Ketteringham', 'fketteringham1c@dailymail.co.uk', 'Female');
+insert into emplooye (id, first_name, last_name, email, gender) values (50, 'Gustav', 'Legging', 'glegging1d@list-manage.com', 'Male');
